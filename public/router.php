5350e654ca3a714e8eae9cc2d16510f7c3202f0a
Bagus Aditya Hermawan <bagus.aditya.h.06@gmail.com>
2026-06-24 18:08:46 +0700

    menambahkan

--- public/router.php
+++ public/router.php
@@ -11,5 +11,6 @@ chdir(__DIR__);
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/index.php';
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['PHP_SELF'] = '/index.php';
+$_SERVER['PATH_INFO'] = $uri; // tambahkan ini
 
 require __DIR__ . '/index.php';

19709f67985bfc8d8c882f7b0fc8f54e4538a35c
Bagus Aditya Hermawan <bagus.aditya.h.06@gmail.com>
2026-06-24 17:53:56 +0700

    menambahkan

--- backend-api/public/router.php
+++ backend-api/public/router.php
@@ -2,10 +2,14 @@
 
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
+if ($uri !== '/' && file_exists(__DIR__ . '/' . ltrim($uri, '/'))) {
     return false;
 }
 
-$_SERVER['SCRIPT_NAME'] = '/index.php';
+chdir(__DIR__);
 
-require_once __DIR__ . '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
+require __DIR__ . '/index.php';

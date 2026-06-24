e968ecc6276050a6900b73c7b50877e179ea984e
Bagus Aditya Hermawan <bagus.aditya.h.06@gmail.com>
2026-06-24 20:57:31 +0700

    menambahkan file

--- backend-api/public/router.php
+++ backend-api/public/router.php
@@ -2,14 +2,13 @@
 
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-if ($uri !== '/' && file_exists(__DIR__ . '/' . ltrim($uri, '/'))) {
+// Serve static files langsung
+if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
     return false;
 }
 
-chdir(__DIR__);
-
-$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/index.php';
+// Forward semua ke index.php
 $_SERVER['SCRIPT_NAME'] = '/index.php';
-$_SERVER['PHP_SELF'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php' . $uri;
 
-require __DIR__ . '/index.php';
+require_once __DIR__ . '/index.php';

be8fe12979e5b6632607f69d670cefca6d1c1a41
Bagus Aditya H <bagus.aditya.h.99@gmail.com>
2026-06-28 12:18:58 +0700

    Update Login.js

--- frontend-spa/pages/Login.js
+++ frontend-spa/pages/Login.js
@@ -70,7 +70,7 @@ template: `
   methods: {
     login() {
       axios.post(apiUrl + '/api/login', {
-        username: this.username,
+        email: this.email,
         password: this.password
       })
         .then(res => {
